9ec2379531ee8fff6c51fc2ef724145725263ef2
Timur Badretdinov <destinerx@gmail.com>
2020-08-28 11:50:50 +0300

    Set EVM target to istanbul

--- truffle-config.js
+++ truffle-config.js
@@ -61,7 +61,7 @@ module.exports = {
                     enabled: true,
                     runs: 200,
                 },
-                evmVersion: 'byzantium',
+                evmVersion: 'istanbul',
             },
         },
     },
