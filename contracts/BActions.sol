11d5bbbef77e9bef342ac6032975b5a0baee0a45
Timur Badretdinov <destinerx@gmail.com>
2021-02-10 19:54:58 +0300

    Send missing tokens back during full migration

--- contracts/BActions.sol
+++ contracts/BActions.sol
@@ -458,6 +458,13 @@ contract BActions {
                 poolOutAmountMin
             )
         );
+        // Send missing tokens back
+        for (uint i = 0; i < tokens.length; i++) {
+            ERC20 token = ERC20(tokens[i]);
+            if (token.balanceOf(address(this)) > 0) {
+                require(token.transfer(msg.sender, token.balanceOf(address(this))), "ERR_TRANSFER_FAILED");
+            }
+        }
     }
     
     // --- Internals ---
